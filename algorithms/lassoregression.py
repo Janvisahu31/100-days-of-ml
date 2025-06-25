7f1983b91423213f57c6bbe20ec471532b754db7
janvisahu31 <sahujanvi317@gmail.com>
2025-06-25 22:48:13 +0530

    lasso regression regularization algorithm

---
+++ algorithms/lassoregression.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+class LassoRegression:
+    def __init__(self, lr=0.01, epochs=1000, alpha=1.0):
+        self.lr = lr
+        self.epochs = epochs
+        self.alpha = alpha
+
+    def fit(self, X, y):
+        self.W = np.zeros(X.shape[1])
+        self.b = 0
+
+        for _ in range(self.epochs):
+            y_pred = np.dot(X, self.W) + self.b
+            error = y_pred - y
+            self.W -= self.lr * (np.dot(X.T, error) + self.alpha * np.sign(self.W))
+            self.b -= self.lr * np.sum(error)
+
+    def predict(self, X):
+        return np.dot(X, self.W) + self.b
